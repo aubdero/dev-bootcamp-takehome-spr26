1806ecb8339eb2e3690bcf0c89e3212b78137777
aubdero <aubderobertis@gmail.com>
2026-08-17 20:30:49 -0400

    Build out personal site: bio, photo, and contact links

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToLinkedIn() {
+    window.location.href = "https://www.linkedin.com/in/aubdero";
+}
+
+function sendEmail() {
+    window.location.href = "mailto:aubdero@gatech.edu";
+}
+
+function goToBlog() {
+    window.location.href = "https://youhavetosaveyourlife.com";
+}
